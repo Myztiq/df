8a0a3f5af775bcf6d2247ac960c079562367366e
Myztiq <github@ryan-kahn.com>
2015-10-09 00:16:10 -0700

    Added br.

--- app/components/greeting1.jsx
+++ app/components/greeting1.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router'
 export default class Greeting1 extends React.Component {
   render() {
     return <div>Greeting1 {this.props.name}
+      <br/>
       <Link to="/inbox/messages/Ryan">Say Hi to Ryan</Link>
       <br/>
       <Link to="/inbox/messages/Sammy">Say Hi to Sammy</Link>
