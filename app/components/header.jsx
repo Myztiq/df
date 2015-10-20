0793482646ebc6c41607295b13ac08c06ed7d29b
Myztiq <github@ryan-kahn.com>
2015-10-19 22:59:43 -0700

    Updated so navigation is dynamic

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -1,27 +1,36 @@
 import React from "react";
 import logo from "images/logo.png"
 import { Link } from 'react-router'
+import auth from "services/auth.js"
 
 export default class Header extends React.Component {
   render() {
+    var loggedIn = auth.loggedIn();
+
+    var navbar = <ul className="nav navbar-nav pull-right">
+      <li className="nav-item">
+        <Link className="nav-link" to="/login">Login</Link>
+      </li>
+      <li className="nav-item">
+        <Link className="nav-link" to="/register">Register</Link>
+      </li>
+    </ul>;
+    if (loggedIn) {
+      navbar = <ul className="nav navbar-nav pull-right">
+        <li className="nav-item">
+          <Link className="nav-link" to="/dashboard">Home</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/setup">Dashboard</Link>
+        </li>
+      </ul>;
+    }
+
     return <header id="header">
       <div className="container">
         <nav className="navbar navbar-dark">
           <a className="navbar-brand" href="/"><img src={logo} /></a>
-          <ul className="nav navbar-nav pull-right">
-            <li className="nav-item active">
-              <Link className="nav-link" to="/dashboard">Home</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/login">Login</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/register">Register</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/setup">Dashboard</Link>
-            </li>
-          </ul>
+          {navbar}
         </nav>
       </div>
     </header>;
