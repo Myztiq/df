27147b7e887625427dd5ba64676072c3f1aeefc7
Myztiq <github@ryan-kahn.com>
2015-11-21 12:28:28 -0800

    Added more logic for retirement dashboard.

--- app/components/dashboard.jsx
+++ app/components/dashboard.jsx
@@ -3,48 +3,169 @@ import { Link } from 'react-router'
 import 'components/setup/dashboard.scss'
 
 export default class extends React.Component {
-  render() {
-    return <div>
+  constructor(props) {
+    super(props);
+    this.state = {
+      retirementAge: 67,
+      retirementAgeSaved: false,
+      paycheckAmount: 110,
+      paycheckAmountSaved: false,
+      fundSaved: false
+    };
+    this.paycheckTotal = 1000;
+    this.companyMatchMax = 200;
+    this.dangerZone = 50;
+    this.recommended = 500;
+  }
 
+  changeRetirementAge = (event)=> {
+    this.setState({
+      retirementAge: parseInt(event.target.value, 10),
+      retirementAgeSaved: false
+    });
+  };
+  changePaycheckAmount = (event)=> {
+    this.setState({
+      paycheckAmount: parseInt(event.target.value, 10),
+      paycheckAmountSaved: false
+    })
+  };
+  getPaycheckPercentage = ()=> {
+    return Math.floor(this.state.paycheckAmount/this.paycheckTotal * 100);
+  };
+
+  getTotalWithCompanyMatch = (myContribution)=> {
+    let companyMatch = this.companyMatchMax;
+    if (myContribution < companyMatch) {
+      companyMatch = myContribution;
+    }
+    return myContribution + companyMatch;
+  };
+
+  render() {
+    var getSaveButton = (target, level)=> {
+      level = level || 'btn-success';
+      var save = ()=> {
+        var saveObj = {};
+        saveObj[target] = true;
+        this.setState(saveObj);
+      };
+      if (this.state[target]) {
+        return
+      }
+      return <div className="input-group-btn">
+        <div className={'btn ' + level}  onClick={save}>
+          <i className="fa fa-check"></i>
+        </div>
+      </div>;
+    };
+    var getFinalSaveButton = ()=> {
+      if (this.state.retirementAgeSaved && this.state.paycheckAmountSaved && this.state.fundSaved) {
+        return <div className="text-right">
+          <div className="btn btn-primary">Save</div>
+        </div>;
+      }
+    };
+    var getCardClassName = (currentSaved, dependenciesSaved)=> {
+      var base = 'sub-card ';
+      if (currentSaved) {
+        return base;
+      }
+      if (!dependenciesSaved) {
+        return base + 'disabled';
+      }
+      return base;
+    };
+
+
+    var paycheckButtonLevel = 'btn-success';
+    var paycheckSubText = '';
+    // Calculate possible states
+    var inDanger = this.state.paycheckAmount <= this.dangerZone;
+    var belowRecommended = this.getTotalWithCompanyMatch(this.state.paycheckAmount) < this.recommended;
+    var belowMatch = this.state.paycheckAmount < this.companyMatchMax;
+
+    if (inDanger) {
+      paycheckButtonLevel = 'btn-danger';
+      paycheckSubText = <div className="sub">
+          Having trouble saving for retirement?
+          <a href="foo" target="_blank">How can we help?</a>
+        </div>;
+    } else if (belowMatch && belowRecommended) {
+      paycheckButtonLevel = 'btn-warning';
+      let myRecommendedContribution = this.recommended/2;
+      let totalWithCompanyMatch = this.getTotalWithCompanyMatch(myRecommendedContribution);
+      if (totalWithCompanyMatch < this.recommended) {
+        myRecommendedContribution += this.recommended - totalWithCompanyMatch;
+      }
+      let remainingMe = myRecommendedContribution - this.state.paycheckAmount;
+
+      let totalMoreNeeded = this.recommended - this.getTotalWithCompanyMatch(this.state.paycheckAmount);
+
+      paycheckSubText = <div className="sub">
+          We recommend you save ${totalMoreNeeded} more a month, your company is willing to match ${totalMoreNeeded - remainingMe} more to help you meet this goal.
+        </div>;
+    } else if (belowMatch) {
+      let remainingCompany = this.companyMatchMax - this.state.paycheckAmount;
+      paycheckButtonLevel = 'btn-warning';
+      paycheckSubText = <div className="sub">
+          You are not maximizing your companies match, leaving ${remainingCompany} on the table.
+        </div>;
+    } else if (belowRecommended) {
+      paycheckButtonLevel = 'btn-warning';
+      paycheckSubText = <div className="sub">
+          Based on your age and amount saved we recommend you save at least ${this.recommended - this.companyMatchMax - this.state.paycheckAmount} more per paycheck.
+        </div>;
+    } else {
+      paycheckSubText = <div className="sub">
+        Good job! You are saving {this.getPaycheckPercentage()}% of your paycheck every 2 weeks.
+        </div>;
+    }
+
+
+
+    return <div id="retirementDashboard">
       <div className="card card-block">
-        <h3 className="card-header">Retirement</h3>
+        <h3>Retirement</h3>
         <div className="card-body">
           <div className="row">
             <div className="col-xs-4">
               <div className="sub-card">
-                <div className="title">I plan on saving</div>
-                <div className="picker btn btn-secondary" data-toggle="modal" data-target="#savingsAmount">
-                  10%
-                  <i className="fa fa-caret-down"></i>
+                <div className="title">I plan to retire when I'm</div>
+                <div className={this.state.retirementAgeSaved ? '' : 'input-group'}>
+                  <input type="number" className="form-control" value={this.state.retirementAge} onChange={this.changeRetirementAge}/>
+                  {getSaveButton('retirementAgeSaved')}
                 </div>
-                <div className="sub">$210 every 2 weeks</div>
+                <div className="sub">The average retirement age is 67</div>
               </div>
             </div>
             <div className="col-xs-4">
-              <div className="sub-card">
-                <div className="title">into</div>
-                <div className="picker btn btn-secondary" data-toggle="modal" data-target="#fund">
-                  Target Date Fund
-                  <i className="fa fa-caret-down"></i>
+              <div className={getCardClassName(this.state.paycheckAmountSaved, this.state.retirementAgeSaved)}>
+                <div className="title">Each paycheck I save</div>
+                <div className='input-group'>
+                  <span className="input-group-addon">$</span>
+                  <input type="number" className="form-control" value={this.state.paycheckAmount} onChange={this.changePaycheckAmount} min="0" max={this.paycheckTotal}/>
+                  {getSaveButton('paycheckAmountSaved', paycheckButtonLevel)}
+                </div>
+                {paycheckSubText}
+              </div>
+            </div>
+            <div className="col-xs-4">
+              <div className={getCardClassName(this.state.fundSaved, this.state.paycheckAmountSaved && this.state.retirementAgeSaved)}>
+                <div className="title">Invested in</div>
+                <div className={this.state.fundSaved ? '' : 'input-group'}>
+                  <div className="picker btn btn-secondary form-control" data-toggle="modal" data-target="#fund">
+                    Target Date Fund
+                  </div>
+                  {getSaveButton('fundSaved')}
                 </div>
                 <div className="sub">
                   <a href="foo">Fund Details</a>
                 </div>
               </div>
             </div>
-            <div className="col-xs-4">
-              <div className="sub-card">
-                <div className="title">retiring at age</div>
-                <div className="picker btn btn-secondary">
-                  67
-                  <i className="fa fa-caret-down"></i>
-                </div>
-              </div>
-            </div>
           </div>
-          <div className="text-right">
-            <div className="btn btn-primary">Save</div>
-          </div>
+          {getFinalSaveButton()}
         </div>
       </div>
 
